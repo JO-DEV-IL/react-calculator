fbfcaaa2316a8966ed425c2b67c21adf993c5c2b
Jillian O'Keefe <jillianokeefe.dev@gmail.com>
2022-10-19 09:58:10 -0400

    working on calculator mechanics

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 //import stylesheet
+import { useReducer } from "react";
 import "./styles.css"
+
+ 
+//calculator 'actions' object for reducer() param
+//exported to DigitButtons.js
+export const ACTIONS ={
+  ADD_DIGIT: 'add-digit',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  EVALUATE: 'evaluate'
+}
+
+//reducer() is a react function
+//more info below about it
+function reducer(state, { type, payload }){
+  //switchcase format to handle state types quickly
+  switch(type){
+    case ACTIONS.ADD_DIGIT:
+      return{ //return new state object
+        ...state, //go through current state
+        currentOperand: `${currentOperand || ""}${payload.digit}`
+      }
+  }
+}
 
 //app frontend
 function App() {
+
+  //Alternative to useState hook
+  //Accepts a reducer of type (reducer()) and returns current state w/ a dispatch method(empty obj for now{})
+  //runs 'reducer' function above
+  //makes doing complex states easier
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer,{})
+  
+  dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit: 1 }})
+
+  // html rendering
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand">8,675,309 +</div>
-        <div className="current-operand">20</div>
+        <div className="previous-operand">{previousOperand} {operation}</div>
+        <div className="current-operand">{currentOperand}</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
