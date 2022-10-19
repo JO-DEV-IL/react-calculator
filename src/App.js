29ea1553f7546b54648e69a131164526fef49a16
Jillian O'Keefe <jillianokeefe.dev@gmail.com>
2022-10-19 09:13:02 -0400

    css updates
    might change the style later

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="previous-operand"></div>
-        <div className="current-operand"></div>
+        <div className="previous-operand">8,675,309 +</div>
+        <div className="current-operand">20</div>
       </div>
       <button className="span-two">AC</button>
       <button>DEL</button>
