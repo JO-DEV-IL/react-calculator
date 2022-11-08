6709be5dd44d7c762defe15cd0789f9ae9832509
JO-DEV-IL <jillianokeefe.dev@gmail.com>
2022-11-07 19:06:36 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+//React dependencies
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-ReactDOM.render(
+
+//ReactDom.render NO LONGER SUPPORTED!!
+//Below is new code to render app to root
+//new import is: import { createRoot } from 'react-dom/client'
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
